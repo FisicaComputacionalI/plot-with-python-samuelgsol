c6ad3ee2ed8e8ce9ec802065155393fa8a2bac10
samuelgsol <31415770+samuelgsol@users.noreply.github.com>
2017-08-28 12:51:45 -0500

    Add files via upload
    archivo editado, numero de hermanos por año

--- temp.py
+++ temp.py
@@ -1,9 +1,9 @@
-#Python usa librerias como matplotlib con objetos predeterminados como pyplot que tienen propiedades que permiten enviar las instrucciones a la maquina de una forma resumida. Matplotlib es una libreria para crear graficas en 2 dimensiones. 
+00#Python usa librerias como matplotlib con objetos predeterminados como pyplot que tienen propiedades que permiten enviar las instrucciones a la maquina de una forma resumida. Matplotlib es una libreria para crear graficas en 2 dimensiones. 
 import matplotlib.pyplot as plt
 #Construimos el objeto plt introduciendo valores que se grafican contra el numero de entrada. Por ejemplo si queremos graficar el crecimiento de un material por dia en centimetros. 
-plt.plot([1,2,3,4])
+plt.plot([1,1,1,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3])
 #Con esta isntruccion colocamos un titulo al eje Y de la grafica. 
-plt.ylabel('Crecimiento del material X por dia [cm]')
+plt.ylabel('numero de hermanos partir de mi nacimiento')
 #Con esta isntruccion guardamos la imagen con el formato que queramos.
 plt.savefig('temp.png')
 #Con esta instruccion mostramos la figura. 
